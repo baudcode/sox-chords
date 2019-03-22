5c00024c8a6d84816e85ee18a5763e2a92eda1bb
Malte Koch <justus.jonas87@googlemail.com>
2019-03-22 19:44:57 +0100

    Added major and minor chord progression helper fns

--- sox_chords/music/spezial_chords.py
+++ sox_chords/music/spezial_chords.py
@@ -1,8 +1,35 @@
 
 import itertools
 import numpy as np
-from sox_chords.music.utils import get_core_notes, get_note
+from sox_chords.music.utils import get_note, get_semi_tone, get_core_from_diff
 from sox_chords.music.chord import Chord
+from sox_chords.music.chords import triad
+from sox_chords.music.base import CoreNote, Note
+from sox_chords.values import Values
+
+def _get_chord_progression(root_note, chord_classes, steps=Values.SEMI_TONE_STEPS):
+    progression = [chord_classes[0](root_note)]
+
+    note = root_note
+    for k, chord in enumerate(chord_classes[1:]):
+        next_core = get_core_from_diff(note, 1)
+        next_semi_tone = get_semi_tone(note, steps[k])
+        note = Note.from_core_and_semi_tone(core=next_core, semi_tone=next_semi_tone)
+        progression.append(chord(note))
+    
+    return progression
+    
+def major_chord_progression(root_note):
+    # C = I ii iii IV V vi vii0 viii
+    # Major, Minor, Minor, Major, Major, Minor, Diminished
+    chords = [triad.MajorTriad, triad.MinorTriad, triad.MinorTriad, triad.MajorTriad, triad.MajorTriad, triad.MinorTriad, triad.DiminishedTriad]
+    return _get_chord_progression(root_note, chords, steps=Values.MAJOR_SEMI_TONE_STEPS)
+
+
+def minor_chord_progression(root_note):
+    # i ii0 III iv v VI VII
+    chords = [triad.MinorTriad, triad.DiminishedTriad, triad.MajorTriad, triad.MinorTriad, triad.MinorTriad, triad.MajorTriad, triad.MajorTriad]
+    return _get_chord_progression(root_note, chords, steps=Values.MINOR_SEMI_TONE_STEPS)
 
 
 def guitar_coords(chords):
